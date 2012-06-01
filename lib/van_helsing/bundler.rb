1accc4e723a4d8a585384d0c7c81ea0bd8d3c276
Rico Sta. Cruz <rico@ambiescent.com>
2012-06-01 19:49:38 +0800

    Remove a debug thing.

--- lib/van_helsing/bundler.rb
+++ lib/van_helsing/bundler.rb
@@ -7,7 +7,6 @@ namespace :bundle do
       mkdir -p "#{shared_path}/bundle"
       mkdir -p "#{release_path}/vendor"
       ln -s "#{shared_path}/bundle" "#{bundle_path}"
-      (cd "#{bundle_path}"; ls -la)
       bundle install --without development:test --path "#{bundle_path}" --binstubs bin/ --deployment
     }
   end
