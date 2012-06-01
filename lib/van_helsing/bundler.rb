97424f29577b210f1504936bd179fdd6dad1b1ab
Rico Sta. Cruz <rico@ambiescent.com>
2012-06-01 19:49:22 +0800

    Lightning-fast Bundler.

--- lib/van_helsing/bundler.rb
+++ lib/van_helsing/bundler.rb
@@ -1,9 +1,14 @@
 namespace :bundle do
   desc "Install gem dependencies using Bundler."
   task :install do
+    bundle_path = "#{release_path}/vendor/bundle"
     queue %{
       echo "-----> Installing gem dependencies using Bundler"
-      bundle install --without development:test --path vendor/bundle --binstubs bin/ --deployment
+      mkdir -p "#{shared_path}/bundle"
+      mkdir -p "#{release_path}/vendor"
+      ln -s "#{shared_path}/bundle" "#{bundle_path}"
+      (cd "#{bundle_path}"; ls -la)
+      bundle install --without development:test --path "#{bundle_path}" --binstubs bin/ --deployment
     }
   end
 end
