870ed6a74a7ae21e1107464a495eaf8e6bea0fe2
Rico Sta. Cruz <rico@ambiescent.com>
2012-06-01 20:34:47 +0800

    Support specific git revisions. This makes it slower though (no shallow copies)

--- lib/van_helsing/git.rb
+++ lib/van_helsing/git.rb
@@ -2,23 +2,24 @@ namespace :git do
   desc "Clones the Git repository to the release path."
   task :clone do
     validate_set :repository
-    settings.branch ||= 'master'
+    settings.revision ||= `git rev-parse HEAD`.strip
 
     queue %{
       echo "-----> Initializing path #{release_path}"
       mkdir -p "#{release_path}" &&
-      git clone "#{repository}" "#{release_path}" -b #{branch} --recursive --depth 1 &&
+      git clone "#{repository}" "#{release_path}" -n --recursive &&
+      git checkout "#{revision}" &&
       rm -rf "#{curent_path}.git"
     }
   end
 
-  desc "Tags the current release version."
+  desc "Tags the current release version. (Done after deployment)"
   task :tag_release do
     # TODO: This doesn't work right, you don't even know if the current working
     # directory has the version being deployed
     validate_set :current_version
     settings.release_tag ||= "release/#{current_version}"
 
-    system "git tag #{release_tag} && git push --tags"
+    system "git tag #{release_tag} #{revision} && git push --tags"
   end
 end
