0fe80cc333483cff18eaa3abf367e13ab8e90ddf
Rico Sta. Cruz <rico@ambiescent.com>
2012-06-03 00:50:37 +0800

    Fix rails:assets_precompile not compiling if no older assets found.

--- lib/van_helsing/rails.rb
+++ lib/van_helsing/rails.rb
@@ -22,26 +22,26 @@ namespace :rails do
   desc "Precompiles assets (skips if nothing has changed since the last release)."
   task :'assets_precompile' do
     if ENV['force_assets']
-      invoke :'rails:assets_procompile:force'
+      invoke :'rails:assets_precompile:force'
       return
     end
 
     queue %{
+      # Check if the last deploy has assets built, and if it can be re-used.
       if [ -d "#{current_path}/public/assets" ]; then
-
         count=`(
-          diff -r "#{current_path}/vendor/assets/" "#{release_path}/vendor/assets/" 2>/dev/null;
-          diff -r "#{current_path}/app/assets/" "#{release_path}/app/assets/" 2>/dev/null
+          diff -r "#{current_path}/vendor/assets/" "./vendor/assets/" 2>/dev/null;
+          diff -r "#{current_path}/app/assets/" "./app/assets/" 2>/dev/null
         ) | wc -l`
 
         if [ "$((count))" = "0" ]; then
           echo "-----> Skipping asset precompilation"
-          cp -R "#{current_path}/public/assets" "#{release_path}/public/assets"
-        else
-          echo "-----> Precompiling asset files"
-          #{rake} assets:precompile
+          cp -R "#{current_path}/public/assets" "./public/assets" && exit
         fi
       fi
+
+      echo "-----> Precompiling asset files"
+      #{rake} assets:precompile
     }
   end
 
