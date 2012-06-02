e26cda4dda517464659d5309d42f5c176acb8b0d
Rico Sta. Cruz <rico@ambiescent.com>
2012-06-02 23:16:41 +0800

    Configurable bundle options.

--- lib/van_helsing/bundler.rb
+++ lib/van_helsing/bundler.rb
@@ -1,13 +1,17 @@
+
+settings.bundle_path ||= './vendor/bundle'
+settings.bundle_options ||= %{--without development:test --path "#{bundle_path}" --binstubs bin/ --deployment"}
+
 namespace :bundle do
   desc "Install gem dependencies using Bundler."
   task :install do
-    bundle_path = "#{release_path}/vendor/bundle"
+    bundle_path = "./vendor/bundle"
     queue %{
       echo "-----> Installing gem dependencies using Bundler"
       mkdir -p "#{shared_path}/bundle"
       mkdir -p "#{release_path}/vendor"
       ln -s "#{shared_path}/bundle" "#{bundle_path}"
-      bundle install --without development:test --path "#{bundle_path}" --binstubs bin/ --deployment
+      bundle install #{bundle_options}
     }
   end
 end
