43fb2c9e4b039018fbd230acea3e48f6c7c39137
Rico Sta. Cruz <rico@ambiescent.com>
2012-06-03 01:23:34 +0800

    Rename force_unlock to deploy:force_unlock. Document deploy:force_unlock.

--- lib/van_helsing/default.rb
+++ lib/van_helsing/default.rb
@@ -1,11 +1,12 @@
-task :force_unlock do
-  queue %{echo "-----> Unlocking"}
-  queue %{rm -f "#{deploy_to}/deploy.lock"}
-  queue %{exit 234}
-  run!
-end
-
 namespace :deploy do
+  desc "Forces a deploy unlock."
+  task :force_unlock do
+    queue %{echo "-----> Unlocking"}
+    queue %{rm -f "#{deploy_to}/deploy.lock"}
+    queue %{exit 234}
+    run!
+  end
+
   desc "Links paths set in :shared_paths."
   task :link_shared_paths do
     validate_set :shared_paths
