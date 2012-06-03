bddca92a4f391bba7a9cc2682efd0449358c6e9d
Rico Sta. Cruz <rico@ambiescent.com>
2012-06-03 20:05:39 +0800

    Make 'vh setup' ensure ownership of the deploy_to path.

--- lib/van_helsing/deploy.rb
+++ lib/van_helsing/deploy.rb
@@ -35,6 +35,7 @@ task :setup do
 
   queue %{echo "-----> Setting up"}
   queue echo_cmd %{mkdir -p "#{deploy_to}"}
+  queue echo_cmd %{chown -R `whoami` "#{deploy_to}"}
   queue echo_cmd %{chmod g+r,a+rwx "#{deploy_to}"}
   queue echo_cmd %{mkdir -p "#{releases_path}"}
   queue echo_cmd %{mkdir -p "#{shared_path}"}
