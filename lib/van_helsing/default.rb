9220670945391aff941cbac57b62d7a5da73e719
Rico Sta. Cruz <rico@ambiescent.com>
2012-06-03 13:58:53 +0800

    Vastly improve 'vh help'.

--- lib/van_helsing/default.rb
+++ lib/van_helsing/default.rb
@@ -19,14 +19,47 @@ end
 
 task :default => :help
 
+def get_tasks(&blk)
+  Rake.application.tasks.select &blk
+end
+
+def print_tasks(tasks, width=nil)
+  name = Rake.application.name
+
+  width ||= tasks.map { |t| t.name_with_args.length }.max || 10
+  tasks.each do |t|
+    if t.comment
+      puts "  #{name} %-#{width}s   # %s" % [ t.name_with_args, t.comment ]
+    else
+      puts "  #{name} %s" % [ t.name_with_args ]
+    end
+  end
+end
+
 desc "Show help."
 task :help do
   name = Rake.application.name
+
+  top_tasks = get_tasks { |t| (! t.name.include? ':') && t.name != 'default' }
+  top_tasks = top_tasks.reject { |t| t.name == 'init' }  if Rake.application.have_rakefile
+
   puts "#{name} - Really fast server deployment and automation tool"
-  puts ""
-  puts "Usage:"
-  puts "  #{name} init      # Creates a sample Van Helsing config file."
-  puts "  #{name} setup     # Sets up a server."
-  puts ""
-  puts "See '#{name} -T' for a full list of available tasks."
+
+  puts "Usage:\n\n"
+  print_tasks top_tasks
+end
+
+desc "Show all tasks."
+task :tasks do
+  sub_tasks = get_tasks { |t| t.name.include? ':' }
+  top_tasks = get_tasks { |t| (! t.name.include? ':') && t.name != 'default' }
+  top_tasks = top_tasks.reject { |t| t.name == 'init' }  if Rake.application.have_rakefile
+
+  puts "Top-level tasks:"
+  print_tasks top_tasks
+
+  if sub_tasks.any?
+    puts "\nMore tasks:"
+    print_tasks sub_tasks
+  end
 end
