1301c8b6b72336052204d0d328ac91c9712bb2dd
Rico Sta. Cruz <rico@ambiescent.com>
2012-06-03 16:19:46 +0800

    Test improvement.

--- spec/commands/command_spec.rb
+++ spec/commands/command_spec.rb
@@ -6,29 +6,30 @@ describe "Invoking the 'vh' command in a project" do
     Dir.chdir root('test_env')
   end
 
-  it 'should print standard help tasks' do
-    vh
+  describe 'without arguments' do
+    before :each do
+      vh
+    end
+    it 'should print standard help tasks' do
+      vh
+      stdout.should include('vh help')
+      stdout.should include('vh init')
+      stdout.should include('vh tasks')
+    end
 
-    stdout.should include('vh help')
-    stdout.should include('vh init')
-    stdout.should include('vh tasks')
+    it 'should print project-specific tasks' do
+      vh
+      stdout.should include('vh deploy')
+      stdout.should include('vh restart')
+      stdout.should include('vh setup')
+    end
 
-  end
+    it "should be the same as running 'help'" do
+      previous_out = stdout
 
-  it 'should print project-specific tasks' do
-    vh
-
-    stdout.should include('vh deploy')
-    stdout.should include('vh restart')
-    stdout.should include('vh setup')
-  end
-
-  it 'should run help when ran without arguments' do
-    vh
-    previous_out = stdout
-
-    vh 'help'
-    stdout.should == previous_out
+      vh 'help'
+      stdout.should == previous_out
+    end
   end
 
   it "with 'vh tasks' should print tasks" do
@@ -46,10 +47,16 @@ describe "Invoking the 'vh' command in a project" do
       vh 'deploy', 'simulate=1'
     end
     
-    it "should include some stuff" do
-      stdout.should include("#{Dir.pwd}/releases")
-      stdout.should include("releases/#{Time.now.strftime('%Y-%m-%d')}")
-      stdout.should =~ /rm -f ".*deploy.lock"/
+    it "take care of the lockfile" do
+      # Lockfile sanity check
+      stdout.should include "another deployment is ongoing"
+      stdout.should =~ /touch ".*deploy\.lock"/
+        stdout.should =~ /rm -f ".*deploy\.lock"/
+    end
+
+    it "should honor release_path" do
+      stdout.should include "#{Dir.pwd}/releases"
+      stdout.should =~ /cd ".*releases\/#{Time.now.strftime('%Y-%m-%d')}/
     end
 
     it "should include deploy directives" do
