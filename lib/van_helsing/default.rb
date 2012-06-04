ae9bc7ac24b9210ae311ced131aebb70c392c3b0
Rico Sta. Cruz <rico@ambiescent.com>
2012-06-05 03:20:42 +0800

    Add --trace to the 'vh help' screen.

--- lib/van_helsing/default.rb
+++ lib/van_helsing/default.rb
@@ -71,9 +71,10 @@ task :help do
     [ "-h, --help", "Show help" ],
     [ "-V, --version", "Show program version" ],
     [ "-v, --verbose", "Show commands as they happen" ],
-    [ "--simulate", "Run in simulation mode" ]
+    [ "    --simulate", "Run in simulation mode" ],
+    [ "-t, --trace", "Show backtraces when errors occur" ]
   ]
-  opts.each { |args| puts "  %-15s %s" % args }
+  opts.each { |args| puts "  %-17s %s" % args }
   puts ""
 
   show_task_help
