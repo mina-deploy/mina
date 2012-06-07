6c531b4dd2a9421ab8e57008e6818785be2de54d
mikong <michaelgalero@gmail.com>
2012-06-07 19:22:54 +0800

    Fix custom config file test.

--- spec/commands/custom_config_spec.rb
+++ spec/commands/custom_config_spec.rb
@@ -4,16 +4,17 @@ require 'command_helper'
 describe "Invoking the 'mina' command in a project without a deploy.rb" do
   before :each do
     Dir.chdir root('spec/fixtures/custom_file_env')
+    FileUtils.rm_rf 'deploy'
   end
 
   it 'should fail' do
     run_command 'deploy', '--simulate'
-    exitstatus.should > 0
+    exitstatus.should be > 0
   end
 
   it 'should pass if you provide a new rakefile' do
     mina 'deploy', '--simulate', '-f', 'custom_deploy.rb'
-    stdout.should.include 'Creating a temporary path'
+    stdout.should include 'Creating a temporary build path'
   end
 end
 
