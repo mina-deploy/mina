4d82d2cddff33e4ea34a5921a0c593f3d5642f0d
Rico Sta. Cruz <rico@ambiescent.com>
2012-08-08 05:12:47 +0800

    Implement :'rvm:use'. See #5, #39.

---
+++ lib/mina/rvm.rb
@@ -0,0 +1,31 @@
+# Common usage:
+#
+#     task :environment do
+#       invoke :'rvm:use[ruby-1.9.3-p125@gemset_name]'
+#     end
+#
+#     task :deploy => :environment do
+#       ...
+#     end
+#
+set_default :rvm_path, "$HOME/.rvm/scripts/rvm"
+
+task :'rvm:use', :env do |t, args|
+  unless args[:env]
+    print_error "Task 'rvm:use' needs an RVM environment name as an argument."
+    print_error "Example: invoke :'rvm:use[ruby-1.9.2@default]'"
+    die
+  end
+
+  queue %{
+    echo "-----> Using RVM environment '#{args[:env]}'"
+    if [[ ! -s "#{rvm_path}" ]]; then
+      echo "! Ruby Version Manager not found"
+      echo "! If RVM is installed, check your :rvm_path setting."
+      exit 1
+    fi
+
+    source #{rvm_path}
+    #{echo_cmd %{rvm use "#{args[:env]}"}} || exit 1
+  }
+end
