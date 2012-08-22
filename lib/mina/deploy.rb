12d56f6761637b8b4522acb74ab95adb6417f59c
Marcus Oladell <mafcocinco@gmail.com>
2012-08-22 13:52:46 -0500

    Changed the cleanup task to sort by directory date rather than name.  This will alow the name of directories within release/ to change without breaking the cleanup task

--- lib/mina/deploy.rb
+++ lib/mina/deploy.rb
@@ -38,7 +38,7 @@ namespace :deploy do
   task :cleanup do
     queue %{echo "-----> Cleaning up old releases (keeping #{keep_releases!})"}
     queue echo_cmd %{cd "#{deploy_to!}/#{releases_path!}" || exit 15}
-    queue echo_cmd %{count=`ls -1d [0-9]* | sort -rn | wc -l`}
+    queue echo_cmd %{count=`ls -1t | wc -l`}
     queue echo_cmd %{remove=$((count > 5 ? count - #{keep_releases} : 0))}
     queue echo_cmd %{ls -1d [0-9]* | sort -rn | tail -n $remove | xargs rm -rf {}}
   end
