ce588de113e7385373bb8fd96d8be209aad67966
Rico Sta. Cruz <rico@ambiescent.com>
2012-09-06 21:37:03 +0800

    Update the default deploy.rb to add notes about 'mina setup' customizations.

--- data/deploy.rb
+++ data/deploy.rb
@@ -34,6 +34,20 @@ task :environment do
   # invoke :'rvm:use[ruby-1.9.3-p125@default]'
 end
 
+# Put any custom mkdir's in here for when `mina setup` is ran.
+# For Rails apps, we'll make some of the shared paths that are shared between
+# all releases.
+task :setup => :environment do
+  queue! %[mkdir -p "#{deploy_to}/shared/log"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/log"]
+
+  queue! %[mkdir -p "#{deploy_to}/shared/config"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/config"]
+
+  queue! %[touch "#{deploy_to}/shared/config/database.yml"]
+  queue  %[-----> Be sure to edit 'shared/config/database.yml'.]
+end
+
 desc "Deploys the current version to the server."
 task :deploy => :environment do
   deploy do
