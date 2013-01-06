7f7dc17f50b64a3242c98dfee10538d068038ba2
marcosbeirigo <marcosbeirigo@gmail.com>
2013-01-06 08:38:35 -0300

    add task to creat rvm wrappers

--- lib/mina/rvm.rb
+++ lib/mina/rvm.rb
@@ -56,3 +56,35 @@ task :'rvm:use', :env do |t, args|
     #{echo_cmd %{rvm use "#{args[:env]}" --create}} || exit 1
   }
 end
+
+
+# ### rvm:use[]
+# Creates a rvm wrapper for a given executable
+#
+# This is usually placed in the `:setup` task.
+#
+#     task ::setup => :environment do
+#       ...
+#       invoke :'rvm:wrapper[ruby-1.9.3-p125@gemset_name,wrapper_name,binary_name]'
+#     end
+#
+task :'rvm:wrapper', :env, :name, :bin do |t,args|
+  unless args[:env] && args[:name] && args[:bin]
+    print_error "Task 'rvm:wrapper' needs an RVM environment name, an wrapper name and the binary name as arguments"
+    print_error "Example: invoke :'rvm:use[ruby-1.9.2@myapp,myapp,unicorn_rails]'"
+    die
+  end
+
+  queue %{
+    echo "-----> creating RVM wrapper '#{args[:name]}_#{args[:bin]}' using '#{args[:env]}'"
+    if [[ ! -s "#{rvm_path}" ]]; then
+      echo "! Ruby Version Manager not found"
+      echo "! If RVM is installed, check your :rvm_path setting."
+      exit 1
+    fi
+
+    source #{rvm_path}
+    #{echo_cmd %{rvm wrapper #{args[:env]} #{args[:name]} #{args[:bin]} }} || exit 1
+  }
+
+end
