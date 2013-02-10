542a4f81979dc3878d8317ad5038c3ec96aeba5e
Eugene Diachkin <ineu@starlightsys.com>
2013-02-10 22:50:52 +0200

    Properly fixed tmp/restart issue

--- data/deploy.rb
+++ data/deploy.rb
@@ -60,7 +60,7 @@ task :deploy => :environment do
     invoke :'rails:assets_precompile'
 
     to :launch do
-      queue "touch #{deploy_to}/tmp/restart.txt"
+      queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
     end
   end
 end
