fb543c57b508bf03ad1c0041f3265e1c29748826
Bob Van Landuyt <bob.vanlanduyt@gmail.com>
2013-07-03 14:35:37 +0200

    Seperate the crontab per environment
    Needed when the application is running on the same domain in different environments

--- lib/mina/whenever.rb
+++ lib/mina/whenever.rb
@@ -6,22 +6,22 @@ namespace :whenever do
   desc "Clear crontab"
   task :clear do
     queue %{
-      echo "-----> Clear crontab for #{domain}"
-      #{echo_cmd %[cd #{deploy_to!}/#{current_path!} ; bundle exec whenever --clear-crontab #{domain} --set 'environment=#{rails_env}&path=#{deploy_to!}/#{current_path!}']}
+      echo "-----> Clear crontab for #{domain}_#{rails_env}"
+      #{echo_cmd %[cd #{deploy_to!}/#{current_path!} ; bundle exec whenever --clear-crontab #{domain}_#{rails_env} --set 'environment=#{rails_env}&path=#{deploy_to!}/#{current_path!}']}
     }
   end
   desc "Update crontab"
   task :update do
     queue %{
-      echo "-----> Update crontab for #{domain}"
-      #{echo_cmd %[cd #{deploy_to!}/#{current_path!} ; bundle exec whenever --update-crontab #{domain} --set 'environment=#{rails_env}&path=#{deploy_to!}/#{current_path!}']}
+      echo "-----> Update crontab for #{domain}_#{rails_env}"
+      #{echo_cmd %[cd #{deploy_to!}/#{current_path!} ; bundle exec whenever --update-crontab #{domain}_#{rails_env} --set 'environment=#{rails_env}&path=#{deploy_to!}/#{current_path!}']}
     }
   end
   desc "Write crontab"
   task :write do
     queue %{
-      echo "-----> Update crontab for #{domain}"
-      #{echo_cmd %[cd #{deploy_to!}/#{current_path!} ; bundle exec whenever --write-crontab #{domain} --set 'environment=#{rails_env}&path=#{deploy_to!}/#{current_path!}']}
+      echo "-----> Update crontab for #{domain}_#{rails_env}"
+      #{echo_cmd %[cd #{deploy_to!}/#{current_path!} ; bundle exec whenever --write-crontab #{domain}_#{rails_env} --set 'environment=#{rails_env}&path=#{deploy_to!}/#{current_path!}']}
     }
   end
 end
