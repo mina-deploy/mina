09aacacb51e5cfb4203974c0ed804c9a5bfd6926
Ted Price <ted.price@gmail.com>
2014-01-10 09:46:29 -0600

    Adding support for environment variables, env_var

--- lib/mina/ssh_helpers.rb
+++ lib/mina/ssh_helpers.rb
@@ -52,7 +52,8 @@ module Mina
       args << " -i #{identity_file}" if identity_file?
       args << " -p #{port}" if port?
       args << " -A" if forward_agent?
-      args << " #{ssh_options}"  if ssh_options?
+      args << " #{ssh_options}" if ssh_options?
+      args << " export #{env_vars}" if env_vars?
       args << " -t"
       "ssh #{args}"
     end
