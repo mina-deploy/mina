60ac6d782d3914ffac05b24663af6a33ebc5a3db
Michael Minter <mothore@gmail.com>
2014-04-22 16:43:58 -0500

    update whenever namespace

--- lib/mina/whenever.rb
+++ lib/mina/whenever.rb
@@ -3,25 +3,25 @@
 # [whenever]: http://rubygems.org/gems/whenever
 
 namespace :whenever do
-  desc "Clear crontab"
+  desc 'Clear crontab'
   task :clear do
     queue %{
-      echo "-----> Clear crontab for #{domain}_#{rails_env}"
-      #{echo_cmd %[cd #{deploy_to!}/#{current_path!} ; bundle exec whenever --clear-crontab #{domain}_#{rails_env} --set 'environment=#{rails_env}&path=#{deploy_to!}/#{current_path!}']}
-    }
+      echo "-----> Clear crontab for #{application}_#{domain}_#{rails_env}"
+      #{echo_cmd %[cd #{deploy_to!}/#{current_path!} ; bundle exec whenever --clear-crontab #{application}_#{domain}_#{rails_env} --set 'environment=#{rails_env}&path=#{deploy_to!}/#{current_path!}']}
+          }
   end
-  desc "Update crontab"
+  desc 'Update crontab'
   task :update do
     queue %{
-      echo "-----> Update crontab for #{domain}_#{rails_env}"
-      #{echo_cmd %[cd #{deploy_to!}/#{current_path!} ; bundle exec whenever --update-crontab #{domain}_#{rails_env} --set 'environment=#{rails_env}&path=#{deploy_to!}/#{current_path!}']}
-    }
+      echo "-----> Update crontab for #{application}_#{domain}_#{rails_env}"
+      #{echo_cmd %[cd #{deploy_to!}/#{current_path!} ; bundle exec whenever --update-crontab #{application}_#{domain}_#{rails_env} --set 'environment=#{rails_env}&path=#{deploy_to!}/#{current_path!}']}
+          }
   end
-  desc "Write crontab"
+  desc 'Write crontab'
   task :write do
     queue %{
-      echo "-----> Update crontab for #{domain}_#{rails_env}"
-      #{echo_cmd %[cd #{deploy_to!}/#{current_path!} ; bundle exec whenever --write-crontab #{domain}_#{rails_env} --set 'environment=#{rails_env}&path=#{deploy_to!}/#{current_path!}']}
-    }
+      echo "-----> Update crontab for #{application}_#{domain}_#{rails_env}"
+      #{echo_cmd %[cd #{deploy_to!}/#{current_path!} ; bundle exec whenever --write-crontab #{application}_#{domain}_#{rails_env} --set 'environment=#{rails_env}&path=#{deploy_to!}/#{current_path!}']}
+          }
   end
 end
