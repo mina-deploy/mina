1b9a8c4eeceb24b73d2536a9050a4759e6cb6238
Postmodern <postmodern.mod3@gmail.com>
2014-09-26 21:25:52 -0700

    Allow the foreman export format to be configurable.
    * Added the foreman_format setting.

--- lib/mina/foreman.rb
+++ lib/mina/foreman.rb
@@ -40,11 +40,12 @@
 set_default :foreman_app,  lambda { application }
 set_default :foreman_user, lambda { user }
 set_default :foreman_log,  lambda { "#{deploy_to!}/#{shared_path}/log" }
+set_default :foreman_format, 'upstart'
 
 namespace :foreman do
   desc 'Export the Procfile to Ubuntu upstart scripts'
   task :export do
-    export_cmd = "sudo bundle exec foreman export upstart /etc/init -a #{foreman_app} -u #{foreman_user} -l #{foreman_log}"
+    export_cmd = "sudo bundle exec foreman export #{foreman_format} /etc/init -a #{foreman_app} -u #{foreman_user} -l #{foreman_log}"
 
     queue %{
       echo "-----> Exporting foreman procfile for #{foreman_app}"
