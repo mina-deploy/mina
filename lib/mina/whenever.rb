e75aad06d7323c795501cc6fa670f83b59ff8bed
Stjepan Hadjic <d4be4st@gmail.com>
2015-07-05 15:02:40 +0200

    fix #267

--- lib/mina/whenever.rb
+++ lib/mina/whenever.rb
@@ -17,21 +17,21 @@
 
 namespace :whenever do
   desc "Clear crontab"
-  task :clear do
+  task :clear => :environment  do
     queue %{
       echo "-----> Clear crontab for #{domain}_#{rails_env}"
       #{echo_cmd %[cd #{deploy_to!}/#{current_path!} ; #{bundle_bin} exec whenever --clear-crontab #{domain}_#{rails_env} --set 'environment=#{rails_env}&path=#{deploy_to!}/#{current_path!}']}
     }
   end
   desc "Update crontab"
-  task :update do
+  task :update => :environment do
     queue %{
       echo "-----> Update crontab for #{domain}_#{rails_env}"
       #{echo_cmd %[cd #{deploy_to!}/#{current_path!} ; #{bundle_bin} exec whenever --update-crontab #{domain}_#{rails_env} --set 'environment=#{rails_env}&path=#{deploy_to!}/#{current_path!}']}
     }
   end
   desc "Write crontab"
-  task :write do
+  task :write => :environment do
     queue %{
       echo "-----> Update crontab for #{domain}_#{rails_env}"
       #{echo_cmd %[cd #{deploy_to!}/#{current_path!} ; #{bundle_bin} exec whenever --write-crontab #{domain}_#{rails_env} --set 'environment=#{rails_env}&path=#{deploy_to!}/#{current_path!}']}
