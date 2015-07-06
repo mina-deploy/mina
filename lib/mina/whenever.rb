148b9e9853766f7b7f24fe311987100c19be064e
Stjepan Hadjic <d4be4st@gmail.com>
2015-07-06 09:08:54 +0200

    added whenever name variable

--- lib/mina/whenever.rb
+++ lib/mina/whenever.rb
@@ -15,26 +15,34 @@
 #       end
 #     end
 
+# ## Settings
+# Any and all of these settings can be overriden in your `deploy.rb`.
+
+# ### whenever_name
+# Sets the name for whenever_name.
+
+set_default :whenever_name, "#{domain}_#{rails_env}"
+
 namespace :whenever do
   desc "Clear crontab"
   task :clear => :environment  do
     queue %{
-      echo "-----> Clear crontab for #{domain}_#{rails_env}"
-      #{echo_cmd %[cd #{deploy_to!}/#{current_path!} ; #{bundle_bin} exec whenever --clear-crontab #{domain}_#{rails_env} --set 'environment=#{rails_env}&path=#{deploy_to!}/#{current_path!}']}
+      echo "-----> Clear crontab for #{whenever_name}"
+      #{echo_cmd %[cd #{deploy_to!}/#{current_path!} ; #{bundle_bin} exec whenever --clear-crontab #{whenever_name} --set 'environment=#{rails_env}&path=#{deploy_to!}/#{current_path!}']}
     }
   end
   desc "Update crontab"
   task :update => :environment do
     queue %{
-      echo "-----> Update crontab for #{domain}_#{rails_env}"
-      #{echo_cmd %[cd #{deploy_to!}/#{current_path!} ; #{bundle_bin} exec whenever --update-crontab #{domain}_#{rails_env} --set 'environment=#{rails_env}&path=#{deploy_to!}/#{current_path!}']}
+      echo "-----> Update crontab for #{whenever_name}"
+      #{echo_cmd %[cd #{deploy_to!}/#{current_path!} ; #{bundle_bin} exec whenever --update-crontab #{whenever_name} --set 'environment=#{rails_env}&path=#{deploy_to!}/#{current_path!}']}
     }
   end
   desc "Write crontab"
   task :write => :environment do
     queue %{
-      echo "-----> Update crontab for #{domain}_#{rails_env}"
-      #{echo_cmd %[cd #{deploy_to!}/#{current_path!} ; #{bundle_bin} exec whenever --write-crontab #{domain}_#{rails_env} --set 'environment=#{rails_env}&path=#{deploy_to!}/#{current_path!}']}
+      echo "-----> Update crontab for #{whenever_name}"
+      #{echo_cmd %[cd #{deploy_to!}/#{current_path!} ; #{bundle_bin} exec whenever --write-crontab #{whenever_name} --set 'environment=#{rails_env}&path=#{deploy_to!}/#{current_path!}']}
     }
   end
 end
