e211a9399ecf974d66505e569e9188f52d820b38
Sunny Ripert <sunny@sunfox.org>
2017-03-14 09:33:38 +0100

    Add skip-existing option to example rbenv install
    This helps against a hanging connection because it is waiting for `continue with installation? (y/N)`.

--- data/deploy.rb
+++ data/deploy.rb
@@ -38,7 +38,7 @@ end
 # Put any custom commands you need to run at setup
 # All paths in `shared_dirs` and `shared_paths` will be created on their own.
 task :setup do
-  # command %{rbenv install 2.3.0}
+  # command %{rbenv install 2.3.0 --skip-existing}
 end
 
 desc "Deploys the current version to the server."
