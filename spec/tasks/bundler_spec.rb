82061f4ccc5e12117621b6c74e13ce9e7f407d92
Manfred Stienstra <manfred@fngtps.com>
2020-11-10 19:42:28 +0100

    Use bundle config when bundle_version is 2.

--- spec/tasks/bundler_spec.rb
+++ spec/tasks/bundler_spec.rb
@@ -1,15 +1,35 @@
 require 'spec_helper'
 
 RSpec.describe 'bundler', type: :rake do
-  describe 'bundle:install' do
-    it 'bundle install' do
-      expect { invoke_all }.to output(output_file('bundle_install')).to_stdout
+  context 'version 2' do
+    before(:each) { Mina::Configuration.instance.set(:bundle_version, 2) }
+
+    describe 'bundle:install' do
+      it 'bundle install' do
+        expect { invoke_all }.to output(output_file('bundle_install_2')).to_stdout
+      end
+    end
+
+    describe 'bundle:clean' do
+      it 'bundle clean' do
+        expect { invoke_all }.to output(output_file('bundle_clean')).to_stdout
+      end
     end
   end
 
-  describe 'bundle:clean' do
-    it 'bundle clean' do
-      expect { invoke_all }.to output(output_file('bundle_clean')).to_stdout
+  context 'version 1' do
+    before(:each) { Mina::Configuration.instance.set(:bundle_version, 1) }
+
+    describe 'bundle:install' do
+      it 'bundle install' do
+        expect { invoke_all }.to output(output_file('bundle_install_1')).to_stdout
+      end
+    end
+
+    describe 'bundle:clean' do
+      it 'bundle clean' do
+        expect { invoke_all }.to output(output_file('bundle_clean')).to_stdout
+      end
     end
   end
 end
--- tasks/mina/bundler.rb
+++ tasks/mina/bundler.rb
@@ -1,8 +1,11 @@
 require 'mina/default'
 
+set :bundle_version, 2
 set :bundle_bin, 'bundle'
 set :bundle_path, 'vendor/bundle'
 set :bundle_withouts, 'development test'
+set :bundle_config, -> { { without: fetch(:bundle_withouts), path: fetch(:bundle_path),  deployment: true } }
+set :bundle_install_options, -> { %{--jobs=3 --retry=3} }
 set :bundle_options, -> { %{--without #{fetch(:bundle_withouts)} --path "#{fetch(:bundle_path)}" --deployment} }
 set :shared_dirs, fetch(:shared_dirs, []).push(fetch(:bundle_path))
 
@@ -10,7 +13,14 @@ namespace :bundle do
   desc 'Install gem dependencies using Bundler.'
   task :install do
     comment %{Installing gem dependencies using Bundler}
-    command %{#{fetch(:bundle_bin)} install #{fetch(:bundle_options)}}
+    if fetch(:bundle_version) == 2
+      fetch(:bundle_config).each do |name, value|
+        command %{#{fetch(:bundle_bin)} config set #{name} "#{value}"}
+      end
+      command %{#{fetch(:bundle_bin)} install #{fetch(:bundle_install_options)}}
+    else
+      command %{#{fetch(:bundle_bin)} install #{fetch(:bundle_options)}}
+    end
   end
 
   desc 'Cleans up unused gems in your bundler directory'
