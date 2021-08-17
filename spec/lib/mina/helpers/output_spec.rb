0722fdb0e908faa664ad4735085536adbee6ef77
Lovro Bikić <lovro.bikic@infinum.hr>
2021-08-17 12:27:52 +0200

    Improve test coverage for output helpers (#690)

--- spec/lib/mina/helpers/output_spec.rb
+++ spec/lib/mina/helpers/output_spec.rb
@@ -9,25 +9,29 @@ describe Mina::Helpers::Output do
 
   describe '#print_line' do
     it 'prints normally' do
-      expect{ helper.print_line('ls -al') }.to output("       ls -al\n").to_stdout
+      expect { helper.print_line('ls -al') }.to output("       ls -al\n").to_stdout
     end
 
     it 'prints comment' do
-      expect{ helper.print_line('-> ls -al') }.to output("\e[32m----->\e[0m ls -al\n").to_stdout
+      expect { helper.print_line('-> ls -al') }.to output("\e[32m----->\e[0m ls -al\n").to_stdout
     end
 
     it 'prints error' do
-      expect{ helper.print_line('! ls -al') }.to output(" \e[33m!\e[0m     \e[31mls -al\e[0m\n").to_stdout
+      expect { helper.print_line('! ls -al') }.to output(" \e[33m!\e[0m     \e[31mls -al\e[0m\n").to_stdout
     end
 
     it 'prints command' do
-      expect{ helper.print_line('$ ls -al') }.to output("       \e[36m$\e[0m \e[36mls -al\e[0m\n").to_stdout
+      expect { helper.print_line('$ ls -al') }.to output("       \e[36m$\e[0m \e[36mls -al\e[0m\n").to_stdout
     end
   end
 
   describe '#print_stderr' do
+    it 'prints normally when the message is formatted by assets precompile' do
+      expect { helper.print_stderr('I, [ ls -al') }.to output("       I, [ ls -al\n").to_stdout
+    end
+
     it 'prints stderr' do
-      expect{ helper.print_stderr('ls -al') }.to output("       \e[31mls -al\e[0m\n").to_stdout
+      expect { helper.print_stderr('ls -al') }.to output("       \e[31mls -al\e[0m\n").to_stdout
     end
   end
 end
