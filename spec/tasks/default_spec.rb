cd0eba1268b8a182e4b160865ee81b6982885339
Lovro Bikić <lovro.bikic@infinum.hr>
2021-08-17 12:28:37 +0200

    Test remaining default tasks (#692)

--- spec/tasks/default_spec.rb
+++ spec/tasks/default_spec.rb
@@ -1,10 +1,60 @@
 require 'spec_helper'
 
 RSpec.describe 'default', type: :rake do
+  describe 'environment' do
+    it 'outputs a deprecation warning' do
+      expect { invoke_all }.to output(output_file('environment')).to_stdout
+    end
+  end
+
   describe 'ssh_keyscan_repo' do
     it 'scans ssh' do
       Mina::Configuration.instance.set(:repository, 'git@github.com/exapmle')
       expect { invoke_all }.to output(output_file('ssh_keyscan_repo')).to_stdout
+    end
+  end
+
+  describe 'ssh_keyscan_domain' do
+    subject { rake['ssh_keyscan_domain'] }
+
+    context "when domain isn't set" do
+      around do |example|
+        original_domain = Mina::Configuration.instance.fetch(:domain)
+        Mina::Configuration.instance.remove(:domain)
+        example.run
+        Mina::Configuration.instance.set(:domain, original_domain)
+      end
+
+      it 'exits with an error message' do
+        expect do
+          invoke_all
+        end.to raise_error(SystemExit)
+           .and output(/domain must be defined!/).to_stdout
+      end
+    end
+
+    context "when port isn't set" do
+      around do |example|
+        original_port = Mina::Configuration.instance.fetch(:port)
+        Mina::Configuration.instance.remove(:port)
+        example.run
+        Mina::Configuration.instance.set(:port, original_port)
+      end
+
+      it 'exits with an error message' do
+        expect do
+          invoke_all
+        end.to raise_error(SystemExit)
+           .and output(/port must be defined!/).to_stdout
+      end
+    end
+
+    context 'when conditions are met' do
+      it 'scans ssh' do
+        expect do
+          invoke_all
+        end.to output(output_file('ssh_keyscan_domain')).to_stdout
+      end
     end
   end
 
