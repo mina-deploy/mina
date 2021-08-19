ed1bf82a02d8d9319eb658165ced29eb109306d0
Lovro Bikic <lovro.bikic@infinum.hr>
2021-08-19 12:07:47 +0200

    Invoke reset! after each spec

--- spec/support/rake_example_group.rb
+++ spec/support/rake_example_group.rb
@@ -3,8 +3,13 @@ module RakeExampleGroup
 
   let(:rake)         { Rake.application }
   let(:task_name)    { self.class.description }
-  let(:run_commands) { rake['run_commands']}
+  let(:run_commands) { rake['run_commands'] }
+  let(:reset!)       { rake['reset!'] }
   subject            { rake[task_name] }
+
+  after(:each) do
+    reset!.invoke
+  end
 
   def load_default_config
     load_config 'default'
