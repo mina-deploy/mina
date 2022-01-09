d12958fdf04f3428d41002300454eaeadaabed3a
Lovro Bikić <lovro.bikic@infinum.hr>
2022-01-09 12:55:29 +0100

    Refactor CLI options (#707)
    * Refactor sort_options method and add tests
    
    * Update Mina CLI options descriptions

--- lib/mina/application.rb
+++ lib/mina/application.rb
@@ -21,12 +21,12 @@ module Mina
     # :nocov:
 
     def sort_options(options)
-      not_applicable_to_mina = ['quiet', 'silent', 'verbose', 'dry-run']
-      options.reject! do |(switch, *)|
-        switch =~ /--#{Regexp.union(not_applicable_to_mina)}/
-      end
+      options_not_applicable_to_mina = /--#{Regexp.union(['quiet', 'silent', 'verbose', 'dry-run'])}/
 
-      super.push(version, verbose, simulate, debug_configuration_variables, no_report_time)
+      mina_options = options.reject { |(switch, *)| switch =~ options_not_applicable_to_mina }
+      mina_options += [version, verbose, simulate, debug_configuration_variables, no_report_time]
+
+      super(mina_options)
     end
 
     def top_level_tasks
@@ -56,7 +56,7 @@ module Mina
     def verbose
       [
         '--verbose', '-v',
-        'Print more info',
+        'Print a command before its execution.',
         lambda do |_value|
           set(:verbose, true)
         end
@@ -66,7 +66,7 @@ module Mina
     def simulate
       [
         '--simulate', '-s',
-        'Do a simulate run without executing actions',
+        'Run a simulation. All commands will be printed but not executed.',
         lambda do |_value|
           set(:simulate, true)
         end
@@ -76,7 +76,7 @@ module Mina
     def debug_configuration_variables
       [
         '--debug-configuration-variables', '-d',
-        'Display the defined config variables before runnig the tasks.',
+        'Display configuration variables.',
         lambda do |_value|
           set(:debug_configuration_variables, true)
         end
@@ -86,7 +86,7 @@ module Mina
     def no_report_time
       [
         '--no-report-time', nil,
-        'Skip time reporting',
+        "Don't report execution time.",
         lambda do |_value|
           set(:skip_report_time, true)
         end
