318f64c259d1f02350e2b9413c30375dd0201be5
Lovro Bikic <lovro.bikic@infinum.hr>
2022-01-13 13:50:29 +0100

    Add deprecation messages for chruby, rbenv, rvm, and ry tasks

--- tasks/mina/ry.rb
+++ tasks/mina/ry.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+unless fetch(:silence_deprecation_warnings)
+  warn '[DEPRECATION] ry support will be removed from Mina in v2.0.0.'
+  warn '[DEPRECATION] To continue using ry tasks, install `mina-version_managers` gem and'
+  warn "[DEPRECATION] replace `require 'mina/ry'` with `require 'mina/version_managers/ry'`."
+  warn '[DEPRECATION] See https://github.com/mina-deploy/mina-version_managers for more info.'
+  warn '[DEPRECATION] You can silence this message by adding `set :silence_deprecation_warnings, true` to `config/deploy.rb`.'
+  warn ''
+end
+
 set :ry_path, '$HOME/.local'
 
 task :ry, :env do |_, args|
