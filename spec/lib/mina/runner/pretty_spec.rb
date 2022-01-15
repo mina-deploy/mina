641c5c79fa834df217834e07d94610d630bf36af
Lovro Bikic <lovro.bikic@infinum.hr>
2022-01-16 00:56:13 +0100

    Use echo command in pretty spec

--- spec/lib/mina/runner/pretty_spec.rb
+++ spec/lib/mina/runner/pretty_spec.rb
@@ -3,11 +3,11 @@
 require 'spec_helper'
 
 describe Mina::Runner::Pretty do
-  subject(:runner) { described_class.new('true') }
+  subject(:runner) { described_class.new('echo Hello world') }
 
   describe '#run', :suppressed_output do
     it 'executes the script' do
-      expect { runner.run }.to output('').to_stdout
+      expect { runner.run }.to output("       Hello world\n").to_stdout
     end
 
     it 'returns true', :suppressed_output do
